713c892b1f2b27dcfdfc6824c75eb2fd9d328538
bahadirbolatt <bhdrblt5@gmail.com>
2022-06-09 22:51:58 +0300

    notebookfile

--- BTK-OOP.py
+++ BTK-OOP.py
@@ -22,33 +22,97 @@ superKahramanAdi = "Superman"
 superKahramanYasi = 30
 
 
-# In[18]:
+# In[50]:
 
 
 class SuperKahraman():
+    
+    ozelGuc = "Görünmezlik"
+    
     def __init__(self,isim,yas,meslek):
         print("init cagirilidi")
         self.isim = isim
         self.yas = yas
         self.meslek = meslek
+        
+        
+    def ornekMethod(self):
+        print(f"ben süperkahramanım ve meslegim: {self.meslek}")
 
 
-# In[19]:
+# In[51]:
 
 
 superman = SuperKahraman("Superman",30,"gazeteci")
 
 
-# In[20]:
+# In[52]:
 
 
 superman.isim = "Clark Kent"
 
 
-# In[21]:
+# In[53]:
 
 
 superman.isim
 
 
-# In[ ]:
+# In[54]:
+
+
+superman.ozelGuc = "Uçabilirlik"
+
+
+# In[55]:
+
+
+superman.ozelGuc
+
+
+# ## Methods
+
+# In[56]:
+
+
+superman.ornekMethod()
+
+
+# In[101]:
+
+
+class Kopek():
+    
+    yilCarpani = 7
+    
+    def __init__(self,yas=5):
+        self.yas = yas
+        self.insanYasinaCevrilmisAttribute = yas * 7
+    
+    def insanYasiniHesapla(self):
+        return self.yas * Kopek.yilCarpani
+
+
+# In[102]:
+
+
+benimKopek = Kopek()
+
+
+# In[103]:
+
+
+benimKopek.yas
+
+
+# In[104]:
+
+
+benimKopek = Kopek(3)
+
+
+# In[106]:
+
+
+benimKopek.insanYasiniHesapla()
+
